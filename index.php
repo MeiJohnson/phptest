ae9a279dbcc4a565f62edc9da1180876421263f7
MeiJohnson <meikojeiko@gmail.com>
2020-05-14 12:39:36 +0300

    second commit

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
+<html>
+<head>
+<title>h1 tags</title>
+</head>
+<body>
+<h1>Царулкова Анастасия</h1>
 <?php
     require 'vendor/autoload.php';
     use Carbon\Carbon;
-    echo Carbon::now() -> format('j.m.Y h:i');
+    echo Carbon::now() -> format('j.m.Y h:i'); ?>
+</html>
