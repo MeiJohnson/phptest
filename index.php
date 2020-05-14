85bb824511176b6156e239d112290939a0abac06
MeiJohnson <meikojeiko@gmail.com>
2020-05-14 14:41:17 +0300

    8.6th

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
     <head>
         <title>My app</title>
     </head>
-    <script src='main.js'>
+    <script>
+        {
+            document.querySelector('input')
+                .addEventListener('input', e => {
+                fetch('/rates.php?dollars='+e.target.value)
+                    .then(x => x.text)
+                    .then(x => document.querySelector('h5').textContent = x)
+                    .catch(() => document.querySelector('h5').textContent = 'ERR');
+            });
+        }
     </script>
     <body> 
         <?php
