29ff7b4efc033b02936bb0427a159ce293916ec9
MeiJohnson <meikojeiko@gmail.com>
2020-05-14 12:58:04 +0300

    4th

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<html>
-<head>
-<title>h1 tags</title>
-</head>
-<body>
-<h1>Царулкова Анастасия</h1>
 <?php
     require 'vendor/autoload.php';
     use Carbon\Carbon;
-    $data = Carbon::now() -> format('j.m.Y h:i');
-    echo '<h2>' .$data. '</h2>';
-    ?>
-</body>
-</html>
+    echo '<h1>Tsarulkova Anastasia</h1>';
+    $date = Carbon::now() -> format('j.m.Y h:i');
+    echo '<h2>' . $date . '</h2>';
+    
