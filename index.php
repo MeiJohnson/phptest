3d78adfc3f284fb5133260c307a6f1817c6df536
MeiJohnson <meikojeiko@gmail.com>
2020-05-14 14:48:52 +0300

    8.8th

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
     <head>
         <title>My app</title>
     </head>
+    <h2>Конвертер валют</h2>
     <input type='text'>
+    <h5></h5>
     <script>
         {
             document.querySelector('input')
                 .addEventListener('input', e => {
                 fetch('/rates.php?dollars='+e.target.value)
-                    .then(x => x.text)
+                    .then(x => x.text())
                     .then(x => document.querySelector('h5').textContent = x)
                     .catch(() => document.querySelector('h5').textContent = 'ERR');
             });
